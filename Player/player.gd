aa0c269c7a41a9cc73a817d91c01576c79d4e852
Chaeeun147 <chaeeunlim.love@gmail.com>
2025-11-14 22:07:34 -0500

    Firing Cooldown
    Making there a cooldown between the shooting gun bullets

--- Player/player.gd
+++ Player/player.gd
@@ -4,8 +4,10 @@ var plBullet := preload("res://Bullet/bullet.tscn")
 
 @onready var sprite := $Sprite2D
 @onready var firingPositions := $FiringPositions
+@onready var fireDelayTimer : = $FireDelayTime
 
 @export var speed: float = 100
+@export var fireDelay: float = 0.1
 var vel := Vector2(0,0)
 
 func _process(delta):
@@ -17,12 +19,13 @@ func _process(delta):
 		sprite.frame = 1
 		
 # check if we are shooting
-	if Input.is_action_pressed("shoot"):
+	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
+		fireDelayTimer.start(fireDelay)
 		for child in firingPositions.get_children():
 			var bullet := plBullet.instantiate()
 			bullet.global_position = child.global_position
 			get_tree().current_scene.add_child(bullet)
-	
+			
 func _physics_process(delta):
 	var dirVec := Vector2(0,0)
 	if Input.is_action_pressed("move_left"):
