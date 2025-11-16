ec5c420c6d75d76e9e018f730955bc868a4e8f2d
Chaeeun147 <chaeeunlim.love@gmail.com>
2025-11-16 17:55:47 -0500

    Bullet Hit Effect
    Created a hit effect whenever the bullet hits the meteor

--- Bullet/bullet.gd
+++ Bullet/bullet.gd
@@ -1,17 +1,20 @@
 extends Area2D
 
+var pBulletEffect := preload("res://Bullet/bullet_effect.tscn")
+
 @export var speed: float = 500
 
 func _physics_process(delta):
-	position.y -= speed *delta
-
+	position.y -= speed * delta
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
-	pass # Replace with function body.
-
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("damageable"):
+		var bulletEffect := pBulletEffect.instantiate()
+		bulletEffect.position = position
+		get_parent().add_child(bulletEffect)
+		
 		area.damage(1)
 		queue_free( )
